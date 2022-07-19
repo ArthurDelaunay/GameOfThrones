b37549990d2155794f1335d888b20d844342e545
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-19 16:27:02 +0200

    part 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      characters: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await (await fetch("https://thronesapi.com/api/v2/Characters")).json()
+    console.log(response)
+  }
+
+
+
+
+	render() {
+		return(
+			<h1>Game of thrones</h1>
+		)
+	}
 }
 
-export default App;
+export default App
