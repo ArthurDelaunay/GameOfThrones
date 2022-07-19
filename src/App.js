5def78b7f08cd5c39d128c49881c515b7e803cac
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-19 22:07:34 +0200

    bonus1 WIP

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
 import React from 'react'
 import Character from './component/Character'
+import './App.css';
+
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      characters: []
+      characters: [],
+      favorites: [],
     }
   }
 
   async componentDidMount() {
     const response = await (await fetch("https://thronesapi.com/api/v2/Characters")).json()
-    console.log(response)
+    this.setState({
+      characters : response
+    })
+  }
+  componentDidUpdate() {
+    console.log(this.state.favorites)
+    // pour log
   }
 
+  handleFavoriteClick = (character) => {
+    this.setState({
+      favorites: this.state.favorites.concat()
+      // a finir
+    })
+    
+  }
 
-
-
-	render() {
-		return(
-      <section>
-        <h1>Game of thrones</h1>
-        <Character />
-      </section>
-			
-		)
-	}
+    render() {
+      
+        return(
+            <div className="background-color_dark">
+                <h1 className="title">Game of thrones</h1>
+                <div className="">
+                    <div className="">
+                        <section className="d-flex flex-wrap gap-2 justify-content-center align-items-center">
+                            {this.state.characters.map( (character) => {
+                            return (
+                                <Character
+                                    key={character.id}
+                                    name={character.fullName}
+                                    title={character.title}
+                                    image={character.imageUrl}
+                                    favorite={this.handleFavoriteClick}
+                                />
+                            )
+                            })}
+                        </section>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
 }
 
 export default App
--- src/component/Character.js
+++ src/component/Character.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import emptyHeart from "../emptyHeart.png"
+import redFullHeart from "../redFullHeart.png"
 
 class Character extends React.Component {
 	render() {
 		return(
-			<p>Character</p>
+        
+            <article className="card box">
+                <div className="card-body">
+                    <img className="card-img-top" src={this.props.image} alt="character"/>
+                    <div className="">
+                        <h2 className="card-title pt-2">{this.props.name}</h2>
+                        <p className="card-text">{this.props.title}</p>
+                    </div> 
+                </div>
+                <div className="d-flex justify-content-center pb-3">
+                    <img onClick={this.props.favorite} src={emptyHeart} alt="heart" width={"32px"}/>
+                </div>
+            </article>
 		)
 	}
 }
